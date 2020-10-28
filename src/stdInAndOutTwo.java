214aaf7773d683418d7b2b614fc7a582c95fce89
Circle Bro <nada8946@gmail.com>
2020-10-28 23:44:33 +0900

    welcome, std In And Out

--- src/stdInAndOutTwo.java
+++ src/stdInAndOutTwo.java
@@ -1,16 +1,19 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class stdInAndOut {
+public class stdInAndOutTwo {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
     }
 
 }
